1ee005fe43ff1ccd81c2b7488fa8239010555863
Варвара Радько <Tyuiu.RadkoVO@outlook.com>
2025-11-28 17:32:22 +0500

    Task3

---
+++ Tyuiu.RadkoVO.Sprint1.Task3.V18.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.RadkoVO.Sprint1.Task3.V18.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint1.Task3.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double a = 8;
+            double b = 10;
+            double c = 3;
+            double wait = 8.889;
+            var res = ds.HowManySquares(a, b, c);
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.RadkoVO.Sprint1.Task3.V18/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.RadkoVO.Sprint1.Task3.V18.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint1.Task1.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         ");
+            Console.WriteLine("***************************************************************************");
+
+            double a = 8;
+            double b = 10;
+            double c = 3;
+            Console.WriteLine("Сторона A прямоугольника = " + a);
+            Console.WriteLine("Сторона B прямоугольника = " + b);
+            Console.WriteLine("Сторона C квадрата = " + c);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               ");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Количество квадратов, которые можно разместить внутри прямоугольника = " + ds.HowManySquares(a, b, c));
+
+            Console.ReadLine();
+        }
+    }
+}
