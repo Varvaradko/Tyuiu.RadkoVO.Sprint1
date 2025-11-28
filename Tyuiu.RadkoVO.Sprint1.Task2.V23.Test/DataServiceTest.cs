e76f3261766b658f5db55a50beb1601893e220aa
Варвара Радько <Tyuiu.RadkoVO@outlook.com>
2025-11-28 13:37:37 +0500

    Task2

---
+++ Tyuiu.RadkoVO.Sprint1.Task2.V23.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.RadkoVO.Sprint1.Task2.V23.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint1.Task2.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 7;
+            var res = ds.ConvertMinutesToSeconds(x);
+            Assert.AreEqual(420, res);
+        }
+    }
+}
---
+++ Tyuiu.RadkoVO.Sprint1.Task2.V23/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.RadkoVO.Sprint1.Task2.V23.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint1.Task1.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         ");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               ");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("X минут, переведенные в секунды =" + ds.ConvertMinutesToSeconds(x));
+
+            Console.ReadLine();
+        }
+    }
+}
