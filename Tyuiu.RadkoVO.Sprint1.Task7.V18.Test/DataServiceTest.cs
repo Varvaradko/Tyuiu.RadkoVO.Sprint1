17522363432fe0a284a78591648ce89713f0d830
Варвара Радько <Tyuiu.RadkoVO@outlook.com>
2025-11-29 12:16:17 +0500

    Task7

---
+++ Tyuiu.RadkoVO.Sprint1.Task7.V18.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.RadkoVO.Sprint1.Task7.V18.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint1.Task7.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalculate1()
+        {
+            DataService ds = new DataService();
+            double x, y;
+            x = 2; y = 3;
+            double wait = 2.493;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void CheckCalculate2()
+        {
+            DataService ds = new DataService();
+            double x, y;
+            x = 5.4; y = 43.2;
+            double wait = 5.669;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.RadkoVO.Sprint1.Task7.V18/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.RadkoVO.Sprint1.Task7.V18.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint1.Task7.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Радько В. О. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Радько Варвара Олеговна | СМАРТб-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение          *");
+            Console.WriteLine("* по исходным значениям данных, вводимых пользователем.                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                            1+sin^2(x+y)                                 *");
+            Console.WriteLine("*                        z= ------------- +x                              *");
+            Console.WriteLine("*                           |      2x    |                                *");
+            Console.WriteLine("*                         2+|x- ---------|                                *");
+            Console.WriteLine("*                           |   1+x^2*y^2|                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y;
+            Console.WriteLine("Введите значение x:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x, y));
+        }
+    }
+}
