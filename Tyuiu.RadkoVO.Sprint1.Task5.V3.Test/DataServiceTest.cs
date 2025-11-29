8fab520edcba6b183d33f5b47c7a9bdc6eef9370
Варвара Радько <Tyuiu.RadkoVO@outlook.com>
2025-11-29 10:38:08 +0500

    Task5

---
+++ Tyuiu.RadkoVO.Sprint1.Task5.V3.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.RadkoVO.Sprint1.Task5.V3.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint1.Task5.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            int k = 156678;
+            DataService ds = new DataService();
+            int res = ds.Calculate(k);
+
+            int wait = 6;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.RadkoVO.Sprint1.Task5.V3/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.RadkoVO.Sprint1.Task5.V3.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint1.Task5.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***********************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                        *");
+            Console.WriteLine("***********************************************************");
+
+            Console.WriteLine("Введите значение K ");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***********************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                              *");
+            Console.WriteLine("***********************************************************");
+
+            int res = Convert.ToInt32(ds.Calculate(k));
+            Console.WriteLine(res);
+
+            Console.ReadLine();
+        }
+    }
+}
